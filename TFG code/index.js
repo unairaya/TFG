a01050c971142e64466fa9ef118ef1b9049d892c
unairaya <unairaya@gmail.com>
2022-05-09 21:02:16 +0200

    Add files via upload

---
+++ TFG code/index.js
@@ -0,0 +1,163 @@
+// constantes de la página
+// universales
+const html = document.querySelector("html");
+const body = document.querySelector("body");
+const main = document.querySelector("main");
+// inicio
+const inicio = document.getElementById("inicio");
+const colorArray = ["#fff", "#f00", "#0f0", "#ff0", "#f0f", "#0ff"];
+// identify
+const identifyContainer = document.getElementById("identify-container");
+const loginContainer = document.getElementById("login");
+const registerContainer = document.getElementById("register");
+const loginBtn = document.getElementById("login-btn");
+const registerBtn = document.getElementById("register-btn");
+const loadLogin = document.getElementById("load-login");
+const loadRegister = document.getElementById("load-register");
+const toasts = document.getElementById("toasts");
+const notificaciones = [
+	"Rellene todos los campos",
+	"Usuario o contraseña incorrectos",
+	"Usuario ya existente",
+	"Contraseña mínimo 8 caractéres",
+	"Las contraseñas no coinciden",
+];
+
+// variables de la página
+let colorIndex = 0;
+let totalUsers;
+let loginUsername;
+let loginPassword;
+let registerUsername;
+let registerPassword1;
+let registerPassword2;
+
+// eventos
+// inicio
+setTimeout(() => {
+	inicio.style.transform = "scale(1)";
+}, 1);
+
+const cambioColor = setInterval(() => {
+	inicio.style.color = colorArray[colorIndex];
+	colorIndex = colorIndex < colorArray.length ? colorIndex + 1 : 0;
+}, 1000);
+
+const subrayado = setInterval(() => {
+	inicio.classList.toggle("underline");
+}, 500);
+
+inicio.addEventListener("click", () => {
+	inicio.style.display = "none";
+	setTimeout((identifyContainer.style.display = "flex")), 1000;
+	clearInterval(cambioColor);
+	clearInterval(subrayado);
+});
+
+// identify
+loadRegister.addEventListener("click", () => {
+	loginContainer.style.left = "-100%";
+	registerContainer.style.left = "0";
+});
+
+loadLogin.addEventListener("click", () => {
+	loginContainer.style.left = "0";
+	registerContainer.style.left = "100%";
+});
+
+loginBtn.addEventListener("click", () => {
+	const loginUsername = document.getElementById("login-username").value;
+	const loginPassword = document.getElementById("login-password").value;
+	if (!loginUsername || !loginPassword) {
+		mostrarNotificacion(0);
+		return
+	}
+	login(loginUsername, loginPassword)
+});
+
+registerBtn.addEventListener("click", () => {
+	const registerUsername = document.getElementById("register-username").value;
+	const registerPassword1 = document.getElementById("register-password1").value
+	const registerPassword2 = document.getElementById("register-password2").value
+	if (!registerUsername || !registerPassword1 || !registerPassword2) {
+		mostrarNotificacion(0);
+		return
+	}
+	if (registerPassword1.length < 8) {
+		mostrarNotificacion(3)
+		return
+	}
+	if (registerPassword1 !== registerPassword2) {
+		mostrarNotificacion(4)
+		return
+	}
+	register(registerUsername, registerPassword1)
+});
+
+
+// funciones
+function mostrarNotificacion(notificacion) {
+	const notif = document.createElement("div");
+	if (notif) {
+		notif.classList.add("toast");
+		notif.innerText = notificaciones[notificacion];
+		toasts.appendChild(notif);
+		setTimeout(() => {
+			notif.remove();
+		}, 2000);
+	}
+}
+
+// indexedDB
+const indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+let db
+let request = indexedDB.open('database')
+request.onupgradeneeded = function (e) {
+	db = e.target.result
+	let objectStore = db.createObjectStore('users', { keyPath: 'username' })
+	objectStore.createIndex('actual', 'actual')
+}
+request.onsuccess = function (e) {
+	db = e.target.result
+	let objectStore = db.transaction('users', 'readwrite').objectStore('users')
+	objectStore.index('actual').get('true').onsuccess = function (e) {
+		if (e.target.result != undefined) {
+			let data = e.target.result
+			data.actual = 'false'
+			objectStore.put(data)
+		}
+	}
+}
+
+function login(username, password) {
+	let objectStore = db.transaction('users', 'readwrite').objectStore('users')
+	objectStore.get(username).onsuccess = function (e) {
+		if(e.target.result != undefined){
+			if (e.target.result.password === password) {
+				let data = e.target.result
+				data.actual = 'true'
+				objectStore.put(data)
+				window.open("./juegos/menu.html", "_self");
+			} else mostrarNotificacion(1)
+		} else mostrarNotificacion(1)
+	}
+}
+
+function register(registerUsername, registerPassword) {
+	let objectStore = db.transaction('users', 'readwrite').objectStore('users')
+	let getRequest = objectStore.get(registerUsername)
+	getRequest.onsuccess = function(e){
+		console.log(e.target.result)
+		if (e.target.result == undefined) {
+			let user = {
+				username: registerUsername,
+				password: registerPassword,
+				actual: 'true',
+				points: 0
+			}
+			objectStore.add(user).onsuccess = function (e) {
+				window.open("./juegos/menu.html", "_self");
+			}
+		} else mostrarNotificacion(2)
+	}
+}
---
+++ TFG code/juegos/menu.js
@@ -0,0 +1,53 @@
+// menu lateral
+const navBtn = document.querySelector('.nav-button')
+const targets = document.querySelectorAll('.target')
+
+navBtn.addEventListener('click', ()=>{
+    targets.forEach(element => {
+        element.classList.toggle('change')
+    });
+})
+
+// galeria de juegos
+const gamesWrapper = document.getElementById('games-wrapper')
+
+function loadGame(id){
+    alert(id)
+}
+
+// indexedDB
+const indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+let db
+let userPoints
+indexedDB.open('database').onsuccess = function (e) {
+	db = e.target.result
+    db.transaction('users').objectStore('users').index('actual').get('true').onsuccess = function(e){
+        if (e.target.result != undefined){
+            userPoints = e.target.result.points
+            cargarPagina()
+        } else alert('No se pudo recuperar sus datos, por favor recarge la página')
+    }
+}
+
+import juegosJson from './juegos.json' assert {type: 'json'}
+function cargarPagina(){
+    
+    let contenido = ''
+    let juegosCargados = 0
+
+    juegosJson.juegos.forEach(game => {
+        if(game.points <= userPoints){
+            contenido += game.preview + '\n'
+        } else {
+            let restPoints = game.points - userPoints
+            contenido += juegosJson.locked1 + restPoints + juegosJson.locked2
+        }
+        juegosCargados++
+    });
+
+    for(let i = 0; i < 9 - juegosCargados; i++){
+        contenido += juegosJson.soon + '\n'
+    }
+
+    gamesWrapper.innerHTML = contenido
+}
